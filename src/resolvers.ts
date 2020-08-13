722cf5f01e002f6113b4f195a01968d6eec650c2
Nick Askam <asknickfilms@gmail.com>
2020-08-13 00:08:32 -0500

    setting up the postgresql register function

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,7 @@
 import { ResolverMap } from "./types/graphql-utils";
+import { GQL } from "./types/schema";
+import * as bcypt from "bcryptjs";
+import { User } from "./entity/User";
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -6,6 +9,18 @@ export const resolvers: ResolverMap = {
       `Hello ${name || "World"}`,
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {},
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword,
+      });
+
+      await user.save();
+      return true;
+    },
   },
 };
