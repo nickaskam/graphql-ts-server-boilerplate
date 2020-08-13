9e3571d5e03b4923811a0cc427bbe4df4f50d2da
Nick Askam <asknickfilms@gmail.com>
2020-08-12 23:30:22 -0500

    updating the resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,11 @@
-export const resolvers = {
+import { ResolverMap } from "./types/graphql-utils";
+
+export const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`,
+    hello: (_: any, { name }: GQL.IHelloOnQueryArguments) =>
+      `Hello ${name || "World"}`,
+  },
+  Mutation: {
+    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {},
   },
 };
