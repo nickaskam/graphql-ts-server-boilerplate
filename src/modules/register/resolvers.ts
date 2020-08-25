f5474933ddbbf09a449753e8e226f920dd7d37e6
Nick Askam <asknickfilms@gmail.com>
2020-08-25 12:16:59 -0500

    updating server to fix uuid issue

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,6 +1,5 @@
 import * as bcypt from "bcryptjs";
 import * as yup from "yup";
-import { v4 } from "uuid";
 
 import { ResolverMap } from "../../types/graphql-utils";
 import { GQL } from "../../types/schema";
@@ -11,8 +10,8 @@ import {
   emailNotLongEnough,
   passwordNotLongEnough,
 } from "./errorMessages";
-import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
-import { sendEmail } from "../../utils/sendEmail";
+// import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
+// import { sendEmail } from "../../utils/sendEmail";
 
 const schema = yup.object().shape({
   email: yup.string().min(3, emailNotLongEnough).max(255).email(),
@@ -23,8 +22,8 @@ export const resolvers: ResolverMap = {
   Mutation: {
     register: async (
       _,
-      args: GQL.IRegisterOnMutationArguments,
-      { redis, url }
+      args: GQL.IRegisterOnMutationArguments
+      // { redis, url }
     ) => {
       try {
         await schema.validate(args, { abortEarly: false });
@@ -48,19 +47,18 @@ export const resolvers: ResolverMap = {
       }
       const hashedPassword = await bcypt.hash(password, 10);
       const user = User.create({
-        id: v4(),
         email,
         password: hashedPassword,
       });
 
       await user.save();
 
-      if (process.env.NODE_ENV !== "test") {
-        await sendEmail(
-          email,
-          await createConfirmEmailLink(url, user.id, redis)
-        );
-      }
+      // if (process.env.NODE_ENV !== "test") {
+      //   await sendEmail(
+      //     email,
+      //     await createConfirmEmailLink(url, user.id, redis)
+      //   );
+      // }
 
       return null;
     },
