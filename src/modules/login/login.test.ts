d14b978ba38adf5dfd83b7d87a26b8880f38090e
Nick Askam <asknickfilms@gmail.com>
2020-08-25 13:33:08 -0500

    adding login tests

--- src/modules/login/login.test.ts
+++ src/modules/login/login.test.ts
@@ -1,17 +1,19 @@
 import { request } from "graphql-request";
-import { invalidLogin } from "./errorMessages";
+import { invalidLogin, confirmEmailError } from "./errorMessages";
+import { User } from "../../entity/User";
+import { createTypeormConn } from "../../utils/createTypeormConn";
 
-// const email = "tom@bob.com";
-// const password = "asdfasdf";
+const email = "tom@bob.com";
+const password = "asdfasdf";
 
-// const registerMutation = (e: string, p: string) => `
-// mutation {
-//     register(email: "${e}", password: "${p}") {
-//         path
-//         message
-//     }
-// }
-// `;
+const registerMutation = (e: string, p: string) => `
+mutation {
+    register(email: "${e}", password: "${p}") {
+        path
+        message
+    }
+}
+`;
 
 const loginMutation = (e: string, p: string) => `
 mutation {
@@ -22,20 +24,48 @@ mutation {
 }
 `;
 
+beforeAll(async () => {
+  await createTypeormConn();
+});
+
+const loginExpectError = async (e: string, p: string, errMsg: string) => {
+  const response = await request(
+    process.env.TEST_HOST as string,
+    loginMutation(e, p)
+  );
+
+  expect(response).toEqual({
+    login: [
+      {
+        path: "email",
+        message: errMsg,
+      },
+    ],
+  });
+};
+
 describe("login", () => {
   test("email not found send back error", async () => {
-    const response = await request(
+    await loginExpectError("bob@bob.com", "whatever", invalidLogin);
+  });
+
+  test("email not confirmed", async () => {
+    await request(
       process.env.TEST_HOST as string,
-      loginMutation("bob@bob.com", "whatever")
+      registerMutation(email, password)
     );
 
-    expect(response).toEqual({
-      login: [
-        {
-          path: "email",
-          message: invalidLogin,
-        },
-      ],
-    });
+    await loginExpectError(email, password, confirmEmailError);
+
+    await User.update({ email }, { confirmed: true });
+
+    await loginExpectError(email, "asdflkasdjklf", invalidLogin);
+
+    const response = await request(
+      process.env.TEST_HOST as string,
+      loginMutation(email, password)
+    );
+
+    expect(response).toEqual({ login: null });
   });
 });
