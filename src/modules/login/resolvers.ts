366ecbcf98b27aca7f61776b3e0a654b721eb1d2
Nick Askam <asknickfilms@gmail.com>
2020-08-27 16:48:56 -0500

    adding in the ability to log out of multiple computers at once

--- src/modules/login/resolvers.ts
+++ src/modules/login/resolvers.ts
@@ -4,6 +4,7 @@ import { ResolverMap } from "../../types/graphql-utils";
 import { GQL } from "../../types/schema";
 import { User } from "../../entity/User";
 import { invalidLogin, confirmEmailError } from "./errorMessages";
+import { userSessionIdPrefix } from "../../constants";
 
 const errorResponse = [
   {
@@ -17,7 +18,7 @@ export const resolvers: ResolverMap = {
     login: async (
       _,
       { email, password }: GQL.ILoginOnMutationArguments,
-      { session }
+      { session, redis, req }
     ) => {
       const user = await User.findOne({ where: { email } });
 
@@ -42,6 +43,9 @@ export const resolvers: ResolverMap = {
 
       // login successful
       session.userId = user.id;
+      if (req.sessionID) {
+        await redis.lpush(`${userSessionIdPrefix}${user.id}`, req.sessionID);
+      }
 
       return null;
     },
