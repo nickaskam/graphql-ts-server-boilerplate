5f11fa66d893125fce004dfbc8f99d2c9e26c931
Nick Askam <asknickfilms@gmail.com>
2020-08-31 13:31:42 -0500

    adding in the logic for locking the account

--- src/modules/login/resolvers.ts
+++ src/modules/login/resolvers.ts
@@ -3,7 +3,11 @@ import * as bcrypt from "bcryptjs";
 import { ResolverMap } from "../../types/graphql-utils";
 import { GQL } from "../../types/schema";
 import { User } from "../../entity/User";
-import { invalidLogin, confirmEmailError } from "./errorMessages";
+import {
+  invalidLogin,
+  confirmEmailError,
+  forgotPasswordLockedError,
+} from "./errorMessages";
 import { userSessionIdPrefix } from "../../constants";
 
 const errorResponse = [
@@ -31,6 +35,15 @@ export const resolvers: ResolverMap = {
           {
             path: "email",
             message: confirmEmailError,
+          },
+        ];
+      }
+
+      if (user.forgotPasswordLocked) {
+        return [
+          {
+            path: "email",
+            message: forgotPasswordLockedError,
           },
         ];
       }
--- src/modules/logout/resolvers.ts
+++ src/modules/logout/resolvers.ts
@@ -1,23 +1,13 @@
 import { ResolverMap } from "../../types/graphql-utils";
 import { redis } from "../../redis";
-import { userSessionIdPrefix, redisSessionPrefix } from "../../constants";
+import { removeAllUsersSessions } from "../../utils/removeAllUsersSessions";
 
 export const resolvers: ResolverMap = {
   Mutation: {
     logout: async (_, __, { session }) => {
       const { userId } = session;
       if (userId) {
-        const sessionIds = await redis.lrange(
-          `${userSessionIdPrefix}${userId}`,
-          0,
-          -1
-        );
-
-        const promises = [];
-        for (let i = 0; i < sessionIds.length; i += 1) {
-          promises.push(redis.del(`${redisSessionPrefix}${sessionIds[i]}`));
-        }
-        await Promise.all(promises);
+        removeAllUsersSessions(userId, redis);
         return true;
       }
 
